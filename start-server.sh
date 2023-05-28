33861636977f00f31ff1677944c4e5ed96bee91b
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2023-05-28 17:01:56 +1000

    Edit details for docker commands

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd in_the_weeds; python3 manage.py createsuperuser --no-input)
 fi
-(cd in_the_weeds; gunicorn in_the_weeds.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd in_the_weeds; gunicorn in_the_weeds.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
 nginx -g "daemon off;"
